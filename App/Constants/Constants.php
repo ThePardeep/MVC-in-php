8a6e7d98d07a03c5c1d0fa5cc8db9f22d65b93dc
ThePardeep <pardeepsaini7927@gmail.com>
2019-10-12 22:55:53 +0530

    change confi

--- App/Constants/Constants.php
+++ App/Constants/Constants.php
@@ -1,12 +1,18 @@
 <?php 
+$DU = getenv("DU");
+$DP = getenv("DP");
+$DBNAME = getenv("DBNAME");
+
+$RP = getenv("RP");
 define("Home","/");
-define("ROOT_PATH","http://codingkeeda.com");
+define("ROOT_PATH",$RP);
 define("HEADER_PATH","View/headerAndNav.php");
 define("FOOTER_PATH","View/footer.php");
 define('JUMBOTRON_PATH', "View/jumbotron.php");
 define('LOGO_PATH', '../logo.png');
-define('Db_USERNAME','Pardeep_Ck');
-define('Db_PASSWORD','Pardeep72??<<');
-define('Db_HOST','localhost');
-define('Db_NAME','Codingkeeda_blog');
+define('Db_USERNAME',$DU);
+define('Db_PASSWORD',$DP);
+define('Db_HOST','remotemysql.com');
+define('Db_NAME',$DBNAME);
 ?>
+
